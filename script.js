7a4bfec7849002c7742303a7f199f50dc48d8b16
Joseph Reeve <jreeve36@gmail.com>
2022-03-08 15:16:13 -1000

    Added the basic ui elements of the calculator and defined the core arithmetic functions

---
+++ script.js
@@ -0,0 +1,23 @@
+//Math operator functions
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b
+}
+
+function multiply(a, b) {
+    return a * b
+}
+
+function divide(a, b) {
+    return a / b
+}
+
+function operate(a, b, operator) {
+    return operator(a, b)
+}
+
+//Adding event listeners to our number buttons 
