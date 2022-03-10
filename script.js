7ae77eb98bce52fa6f296271d82196bb25b06820
Joseph Reeve <jreeve36@gmail.com>
2022-03-09 15:20:47 -1000

    Now displays numbes above the input to help users remember what they entered previously

--- script.js
+++ script.js
@@ -27,6 +27,7 @@ const display = document.querySelector('.displayResult')
 display.textContent = displayValue
 
 //Adding event listeners to our number and operator buttons 
+
 const zero = document.getElementById('0')
 const one = document.getElementById('1')
 const two = document.getElementById('2')
@@ -105,33 +106,32 @@ clearBtn.addEventListener('click', function(e) {
 function clear() {
     displayValue = ""
     display.textContent = displayValue
+    displayPrev.textContent = ""
+    operationChar = ""
 }
 
-
+//operation buttons event listener
 addBtn.addEventListener('click', function(e) {
     savedValue = parseFloat(displayValue)
     savedOperation = add
     clear()
-    console.log(savedValue + 1)
 })
 subtractBtn.addEventListener('click', function(e) {
     savedValue = parseFloat(displayValue)
     savedOperation = subtract
     clear()
-    console.log(savedValue - 1)
 })
 multiplyBtn.addEventListener('click', function(e) {
     savedValue = parseFloat(displayValue)
     savedOperation = multiply
     clear()
-    console.log(savedValue * 1)
 })
 divideBtn.addEventListener('click', function(e) {
     savedValue = parseFloat(displayValue)
     savedOperation = divide
     clear()
-    console.log(savedValue / 1)
 })
+
 
 equalsBtn.addEventListener('click', function(e) {
     if (parseFloat(displayValue) === 0 && savedOperation == divide) {
@@ -140,4 +140,32 @@ equalsBtn.addEventListener('click', function(e) {
         displayValue = operate(savedValue, parseFloat(displayValue), savedOperation)
         display.textContent = displayValue
     }
+    savedValue = null
+    savedOperation = null
+    displayPrev.textContent = ""
+})
+
+//these will display the previous number and operation entered for a better user experience
+const displayPrev = document.querySelector('.displayPrev')
+let operationChar = ""
+
+
+//display the saved value above the current value after any clicks if there are values to display
+const wholePage = document.querySelector("*")
+wholePage.addEventListener('click', function(e) {
+    switch(savedOperation) {
+        case add: operationChar = "+";
+        break;
+        case subtract: operationChar = "-"
+        break;
+        case multiply: operationChar = "x"
+        break;
+        case divide: operationChar = "/"
+        break;
+        default: null
+    }
+    if (!(savedValue === null || savedOperation === null)) {
+        displayPrev.textContent = savedValue + " " + operationChar
+    }
+    
 })
